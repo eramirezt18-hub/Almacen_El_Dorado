50853b9f71e20bd3b92653d27335ca5ca12ce4f9
Endso <eramirezt18@miumg.edu.gt>
2026-05-21 01:32:41 -0600

    Se ha añadido el sistema de movimientos

--- Almacen_El_Dorado/FrmMovimientos.Designer.cs
+++ Almacen_El_Dorado/FrmMovimientos.Designer.cs
@@ -28,12 +28,216 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbTipoMovimiento = new System.Windows.Forms.GroupBox();
+            this.rbEntrada = new System.Windows.Forms.RadioButton();
+            this.rbSalida = new System.Windows.Forms.RadioButton();
+            this.gbDatosMovimiento = new System.Windows.Forms.GroupBox();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.cmbProducto = new System.Windows.Forms.ComboBox();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.nudCantidad = new System.Windows.Forms.NumericUpDown();
+            this.lblStockActual = new System.Windows.Forms.Label();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnLimpiarMov = new System.Windows.Forms.Button();
+            this.gbHistorial = new System.Windows.Forms.GroupBox();
+            this.dgvMovimientos = new System.Windows.Forms.DataGridView();
+            this.gbTipoMovimiento.SuspendLayout();
+            this.gbDatosMovimiento.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCantidad)).BeginInit();
+            this.gbHistorial.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMovimientos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbTipoMovimiento
+            // 
+            this.gbTipoMovimiento.Controls.Add(this.rbSalida);
+            this.gbTipoMovimiento.Controls.Add(this.rbEntrada);
+            this.gbTipoMovimiento.Location = new System.Drawing.Point(12, 45);
+            this.gbTipoMovimiento.Name = "gbTipoMovimiento";
+            this.gbTipoMovimiento.Size = new System.Drawing.Size(198, 117);
+            this.gbTipoMovimiento.TabIndex = 0;
+            this.gbTipoMovimiento.TabStop = false;
+            this.gbTipoMovimiento.Text = "Tipo de MOvimiento";
+            this.gbTipoMovimiento.Enter += new System.EventHandler(this.gbTipoMovimiento_Enter);
+            // 
+            // rbEntrada
+            // 
+            this.rbEntrada.AutoSize = true;
+            this.rbEntrada.Location = new System.Drawing.Point(21, 44);
+            this.rbEntrada.Name = "rbEntrada";
+            this.rbEntrada.Size = new System.Drawing.Size(75, 20);
+            this.rbEntrada.TabIndex = 0;
+            this.rbEntrada.TabStop = true;
+            this.rbEntrada.Text = "Entrada";
+            this.rbEntrada.UseVisualStyleBackColor = true;
+            this.rbEntrada.CheckedChanged += new System.EventHandler(this.rbEntrada_CheckedChanged);
+            // 
+            // rbSalida
+            // 
+            this.rbSalida.AutoSize = true;
+            this.rbSalida.Location = new System.Drawing.Point(21, 83);
+            this.rbSalida.Name = "rbSalida";
+            this.rbSalida.Size = new System.Drawing.Size(67, 20);
+            this.rbSalida.TabIndex = 1;
+            this.rbSalida.TabStop = true;
+            this.rbSalida.Text = "Salida";
+            this.rbSalida.UseVisualStyleBackColor = true;
+            this.rbSalida.CheckedChanged += new System.EventHandler(this.rbSalida_CheckedChanged);
+            // 
+            // gbDatosMovimiento
+            // 
+            this.gbDatosMovimiento.Controls.Add(this.btnLimpiarMov);
+            this.gbDatosMovimiento.Controls.Add(this.btnRegistrar);
+            this.gbDatosMovimiento.Controls.Add(this.lblStockActual);
+            this.gbDatosMovimiento.Controls.Add(this.nudCantidad);
+            this.gbDatosMovimiento.Controls.Add(this.lblCantidad);
+            this.gbDatosMovimiento.Controls.Add(this.cmbProducto);
+            this.gbDatosMovimiento.Controls.Add(this.lblProducto);
+            this.gbDatosMovimiento.Location = new System.Drawing.Point(350, 12);
+            this.gbDatosMovimiento.Name = "gbDatosMovimiento";
+            this.gbDatosMovimiento.Size = new System.Drawing.Size(398, 199);
+            this.gbDatosMovimiento.TabIndex = 1;
+            this.gbDatosMovimiento.TabStop = false;
+            this.gbDatosMovimiento.Text = "Datos del Movimiento";
+            this.gbDatosMovimiento.Enter += new System.EventHandler(this.gbDatosMovimiento_Enter);
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(6, 33);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(64, 16);
+            this.lblProducto.TabIndex = 0;
+            this.lblProducto.Text = "Producto:";
+            // 
+            // cmbProducto
+            // 
+            this.cmbProducto.FormattingEnabled = true;
+            this.cmbProducto.Location = new System.Drawing.Point(108, 30);
+            this.cmbProducto.Name = "cmbProducto";
+            this.cmbProducto.Size = new System.Drawing.Size(121, 24);
+            this.cmbProducto.TabIndex = 1;
+            this.cmbProducto.SelectedIndexChanged += new System.EventHandler(this.cmbProducto_SelectedIndexChanged);
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(6, 66);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(64, 16);
+            this.lblCantidad.TabIndex = 2;
+            this.lblCantidad.Text = "Cantidad:";
+            // 
+            // nudCantidad
+            // 
+            this.nudCantidad.Location = new System.Drawing.Point(109, 60);
+            this.nudCantidad.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.nudCantidad.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudCantidad.Name = "nudCantidad";
+            this.nudCantidad.Size = new System.Drawing.Size(120, 22);
+            this.nudCantidad.TabIndex = 3;
+            this.nudCantidad.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudCantidad.ValueChanged += new System.EventHandler(this.nudCantidad_ValueChanged);
+            // 
+            // lblStockActual
+            // 
+            this.lblStockActual.AutoSize = true;
+            this.lblStockActual.Location = new System.Drawing.Point(6, 98);
+            this.lblStockActual.Name = "lblStockActual";
+            this.lblStockActual.Size = new System.Drawing.Size(92, 16);
+            this.lblStockActual.TabIndex = 4;
+            this.lblStockActual.Text = "Stock Actual:--";
+            this.lblStockActual.Click += new System.EventHandler(this.lblStockActual_Click);
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(72, 130);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(157, 29);
+            this.btnRegistrar.TabIndex = 5;
+            this.btnRegistrar.Text = "Registrar Movimiento";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnLimpiarMov
+            // 
+            this.btnLimpiarMov.Location = new System.Drawing.Point(90, 165);
+            this.btnLimpiarMov.Name = "btnLimpiarMov";
+            this.btnLimpiarMov.Size = new System.Drawing.Size(108, 26);
+            this.btnLimpiarMov.TabIndex = 6;
+            this.btnLimpiarMov.Text = "Limpiar";
+            this.btnLimpiarMov.UseVisualStyleBackColor = true;
+            this.btnLimpiarMov.Click += new System.EventHandler(this.btnLimpiarMov_Click);
+            // 
+            // gbHistorial
+            // 
+            this.gbHistorial.Controls.Add(this.dgvMovimientos);
+            this.gbHistorial.Location = new System.Drawing.Point(12, 217);
+            this.gbHistorial.Name = "gbHistorial";
+            this.gbHistorial.Size = new System.Drawing.Size(776, 231);
+            this.gbHistorial.TabIndex = 2;
+            this.gbHistorial.TabStop = false;
+            this.gbHistorial.Text = "Historial de Movimientos";
+            this.gbHistorial.Enter += new System.EventHandler(this.gbHistorial_Enter);
+            // 
+            // dgvMovimientos
+            // 
+            this.dgvMovimientos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMovimientos.Location = new System.Drawing.Point(6, 21);
+            this.dgvMovimientos.Name = "dgvMovimientos";
+            this.dgvMovimientos.RowHeadersWidth = 51;
+            this.dgvMovimientos.RowTemplate.Height = 24;
+            this.dgvMovimientos.Size = new System.Drawing.Size(764, 210);
+            this.dgvMovimientos.TabIndex = 0;
+            this.dgvMovimientos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMovimientos_CellContentClick);
+            // 
+            // FrmMovimientos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbHistorial);
+            this.Controls.Add(this.gbDatosMovimiento);
+            this.Controls.Add(this.gbTipoMovimiento);
+            this.Name = "FrmMovimientos";
             this.Text = "FrmMovimientos";
+            this.gbTipoMovimiento.ResumeLayout(false);
+            this.gbTipoMovimiento.PerformLayout();
+            this.gbDatosMovimiento.ResumeLayout(false);
+            this.gbDatosMovimiento.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCantidad)).EndInit();
+            this.gbHistorial.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMovimientos)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbTipoMovimiento;
+        private System.Windows.Forms.RadioButton rbEntrada;
+        private System.Windows.Forms.RadioButton rbSalida;
+        private System.Windows.Forms.GroupBox gbDatosMovimiento;
+        private System.Windows.Forms.Label lblStockActual;
+        private System.Windows.Forms.NumericUpDown nudCantidad;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.ComboBox cmbProducto;
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.Button btnLimpiarMov;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.GroupBox gbHistorial;
+        private System.Windows.Forms.DataGridView dgvMovimientos;
     }
 }
--- Almacen_El_Dorado/FrmMovimientos.cs
+++ Almacen_El_Dorado/FrmMovimientos.cs
@@ -1,20 +1,412 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Almacen_El_Dorado
 {
     public partial class FrmMovimientos : Form
     {
+        // ========== LISTAS TEMPORALES ==========
+        List<Movimiento> listaMovimientos = new List<Movimiento>();
+        List<ProductoSimple> listaProductos = new List<ProductoSimple>();
+
+        // ========== CONSTRUCTOR ==========
         public FrmMovimientos()
         {
             InitializeComponent();
+
+            // Configurar el NumericUpDown
+            nudCantidad.Minimum = 1;
+            nudCantidad.Maximum = 9999;
+            nudCantidad.Value = 1;
+
+            // Cargar productos de ejemplo
+            CargarProductos();
+
+            // Cargar productos en el ComboBox
+            CargarComboProductos();
+
+            // Cargar movimientos de ejemplo
+            CargarMovimientosEjemplo();
+
+            // Configurar DataGridView
+            ConfigurarDataGridView();
+
+            // Mostrar movimientos
+            MostrarMovimientos();
+
+            // Seleccionar Entrada por defecto
+            rbEntrada.Checked = true;
+
+            // Actualizar stock mostrado
+            ActualizarStockMostrado();
         }
+
+        // ========== CLASE PRODUCTO (SIMPLIFICADA) ==========
+        class ProductoSimple
+        {
+            public int Id { get; set; }
+            public string Codigo { get; set; }
+            public string Nombre { get; set; }
+            public int Stock { get; set; }
+            public decimal Precio { get; set; }
+        }
+
+        // ========== CLASE MOVIMIENTO ==========
+        class Movimiento
+        {
+            public int Id { get; set; }
+            public DateTime Fecha { get; set; }
+            public string Tipo { get; set; }
+            public string ProductoNombre { get; set; }
+            public int Cantidad { get; set; }
+            public int ProductoId { get; set; }
+        }
+
+        // ========== CARGAR PRODUCTOS DE EJEMPLO ==========
+        private void CargarProductos()
+        {
+            listaProductos.Clear();
+
+            listaProductos.Add(new ProductoSimple
+            {
+                Id = 1,
+                Codigo = "PROD001",
+                Nombre = "Laptop HP",
+                Stock = 10,
+                Precio = 5000
+            });
+
+            listaProductos.Add(new ProductoSimple
+            {
+                Id = 2,
+                Codigo = "PROD002",
+                Nombre = "Mouse USB",
+                Stock = 50,
+                Precio = 150
+            });
+
+            listaProductos.Add(new ProductoSimple
+            {
+                Id = 3,
+                Codigo = "PROD003",
+                Nombre = "Camisa Polo",
+                Stock = 30,
+                Precio = 250
+            });
+
+            listaProductos.Add(new ProductoSimple
+            {
+                Id = 4,
+                Codigo = "PROD004",
+                Nombre = "Teclado USB",
+                Stock = 8,
+                Precio = 300
+            });
+
+            listaProductos.Add(new ProductoSimple
+            {
+                Id = 5,
+                Codigo = "PROD005",
+                Nombre = "Silla Gamer",
+                Stock = 3,
+                Precio = 1200
+            });
+        }
+
+        // ========== CARGAR PRODUCTOS EN EL COMBOBOX ==========
+        private void CargarComboProductos()
+        {
+            cmbProducto.Items.Clear();
+
+            foreach (ProductoSimple p in listaProductos)
+            {
+                // Mostrar: ID - Nombre (Stock: X)
+                cmbProducto.Items.Add($"{p.Id} - {p.Nombre} (Stock: {p.Stock})");
+            }
+
+            if (cmbProducto.Items.Count > 0)
+            {
+                cmbProducto.SelectedIndex = 0;
+            }
+        }
+
+        // ========== CARGAR MOVIMIENTOS DE EJEMPLO ==========
+        private void CargarMovimientosEjemplo()
+        {
+            listaMovimientos.Clear();
+
+            listaMovimientos.Add(new Movimiento
+            {
+                Id = 1,
+                Fecha = DateTime.Now.AddDays(-2),
+                Tipo = "ENTRADA",
+                ProductoNombre = "Laptop HP",
+                Cantidad = 5,
+                ProductoId = 1
+            });
+
+            listaMovimientos.Add(new Movimiento
+            {
+                Id = 2,
+                Fecha = DateTime.Now.AddDays(-1),
+                Tipo = "SALIDA",
+                ProductoNombre = "Mouse USB",
+                Cantidad = 3,
+                ProductoId = 2
+            });
+        }
+
+        // ========== CONFIGURAR DATAGRIDVIEW ==========
+        private void ConfigurarDataGridView()
+        {
+            dgvMovimientos.Columns.Clear();
+
+            dgvMovimientos.Columns.Add("Fecha", "Fecha");
+            dgvMovimientos.Columns.Add("Tipo", "Tipo");
+            dgvMovimientos.Columns.Add("Producto", "Producto");
+            dgvMovimientos.Columns.Add("Cantidad", "Cantidad");
+
+            dgvMovimientos.Columns["Fecha"].Width = 130;
+            dgvMovimientos.Columns["Tipo"].Width = 80;
+            dgvMovimientos.Columns["Producto"].Width = 150;
+            dgvMovimientos.Columns["Cantidad"].Width = 80;
+
+            dgvMovimientos.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvMovimientos.AllowUserToAddRows = false;
+        }
+
+        // ========== MOSTRAR MOVIMIENTOS ==========
+        private void MostrarMovimientos()
+        {
+            dgvMovimientos.Rows.Clear();
+
+            int fila = 0;
+            foreach (Movimiento m in listaMovimientos)
+            {
+                dgvMovimientos.Rows.Add();
+                dgvMovimientos.Rows[fila].Cells[0].Value = m.Fecha.ToString("dd/MM/yyyy HH:mm");
+                dgvMovimientos.Rows[fila].Cells[1].Value = m.Tipo;
+                dgvMovimientos.Rows[fila].Cells[2].Value = m.ProductoNombre;
+                dgvMovimientos.Rows[fila].Cells[3].Value = m.Cantidad;
+
+                // Color diferente para entradas y salidas
+                if (m.Tipo == "ENTRADA")
+                {
+                    dgvMovimientos.Rows[fila].DefaultCellStyle.BackColor = System.Drawing.Color.LightGreen;
+                }
+                else
+                {
+                    dgvMovimientos.Rows[fila].DefaultCellStyle.BackColor = System.Drawing.Color.LightCoral;
+                }
+
+                fila++;
+            }
+        }
+
+        // ========== ACTUALIZAR STOCK MOSTRADO ==========
+        private void ActualizarStockMostrado()
+        {
+            if (cmbProducto.SelectedIndex >= 0)
+            {
+                int id = ObtenerIdProductoSeleccionado();
+
+                foreach (ProductoSimple p in listaProductos)
+                {
+                    if (p.Id == id)
+                    {
+                        lblStockActual.Text = $"📦 Stock actual: {p.Stock} unidades";
+
+                        // Cambiar color si stock bajo
+                        if (p.Stock <= 5)
+                        {
+                            lblStockActual.ForeColor = System.Drawing.Color.Red;
+                        }
+                        else
+                        {
+                            lblStockActual.ForeColor = System.Drawing.Color.Black;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        // ========== OBTENER ID DEL PRODUCTO SELECCIONADO ==========
+        private int ObtenerIdProductoSeleccionado()
+        {
+            if (cmbProducto.SelectedIndex >= 0)
+            {
+                string texto = cmbProducto.SelectedItem.ToString();
+                string idTexto = texto.Split('-')[0].Trim();
+                return int.Parse(idTexto);
+            }
+            return 0;
+        }
+
+        // ========== OBTENER PRODUCTO POR ID ==========
+        private ProductoSimple ObtenerProductoPorId(int id)
+        {
+            foreach (ProductoSimple p in listaProductos)
+            {
+                if (p.Id == id)
+                    return p;
+            }
+            return null;
+        }
+
+        // ========== VALIDAR MOVIMIENTO ==========
+        private bool ValidarMovimiento()
+        {
+            // Verificar que haya un producto seleccionado
+            if (cmbProducto.SelectedIndex < 0)
+            {
+                MessageBox.Show(" Seleccione un producto", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            // Verificar cantidad
+            int cantidad = (int)nudCantidad.Value;
+            if (cantidad <= 0)
+            {
+                MessageBox.Show(" La cantidad debe ser mayor a 0", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            // Verificar stock si es salida
+            if (rbSalida.Checked)
+            {
+                int idProducto = ObtenerIdProductoSeleccionado();
+                ProductoSimple producto = ObtenerProductoPorId(idProducto);
+
+                if (producto.Stock < cantidad)
+                {
+                    MessageBox.Show($" No hay suficiente stock\n\n" +
+                        $"Producto: {producto.Nombre}\n" +
+                        $"Stock actual: {producto.Stock}\n" +
+                        $"Cantidad solicitada: {cantidad}",
+                        "Stock insuficiente",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // ========== REGISTRAR MOVIMIENTO ==========
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            // Validar
+            if (!ValidarMovimiento())
+                return;
+
+            // Obtener datos
+            int idProducto = ObtenerIdProductoSeleccionado();
+            ProductoSimple producto = ObtenerProductoPorId(idProducto);
+            int cantidad = (int)nudCantidad.Value;
+            string tipo = rbEntrada.Checked ? "ENTRADA" : "SALIDA";
+
+            // Actualizar stock del producto
+            if (tipo == "ENTRADA")
+            {
+                producto.Stock += cantidad;
+            }
+            else
+            {
+                producto.Stock -= cantidad;
+            }
+
+            // Registrar movimiento
+            Movimiento nuevoMov = new Movimiento();
+            nuevoMov.Id = listaMovimientos.Count + 1;
+            nuevoMov.Fecha = DateTime.Now;
+            nuevoMov.Tipo = tipo;
+            nuevoMov.ProductoNombre = producto.Nombre;
+            nuevoMov.Cantidad = cantidad;
+            nuevoMov.ProductoId = producto.Id;
+
+            listaMovimientos.Add(nuevoMov);
+
+            // Actualizar ComboBox (para mostrar nuevo stock)
+            CargarComboProductos();
+
+            // Seleccionar el mismo producto
+            for (int i = 0; i < cmbProducto.Items.Count; i++)
+            {
+                if (cmbProducto.Items[i].ToString().Contains(idProducto.ToString()))
+                {
+                    cmbProducto.SelectedIndex = i;
+                    break;
+                }
+            }
+
+            // Actualizar stock mostrado
+            ActualizarStockMostrado();
+
+            // Actualizar tabla de movimientos
+            MostrarMovimientos();
+
+            // Limpiar cantidad
+            nudCantidad.Value = 1;
+
+            // Mostrar mensaje de éxito
+            MessageBox.Show($"✅ Movimiento registrado correctamente\n\n" +
+                $" Producto: {producto.Nombre}\n" +
+                $"Tipo: {tipo}\n" +
+                $" Cantidad: {cantidad}\n" +
+                $"Nuevo stock: {producto.Stock}",
+                "Éxito",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        // ========== LIMPIAR CAMPOS ==========
+        private void btnLimpiarMov_Click(object sender, EventArgs e)
+        {
+            nudCantidad.Value = 1;
+            rbEntrada.Checked = true;
+
+            if (cmbProducto.Items.Count > 0)
+                cmbProducto.SelectedIndex = 0;
+
+            ActualizarStockMostrado();
+        }
+
+        // ========== CUANDO CAMBIA EL PRODUCTO SELECCIONADO ==========
+        private void cmbProducto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ActualizarStockMostrado();
+        }
+
+        // ========== CUANDO CAMBIA EL TIPO DE MOVIMIENTO ==========
+        private void rbEntrada_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbEntrada.Checked)
+            {
+                btnRegistrar.BackColor = System.Drawing.Color.LightGreen;
+                btnRegistrar.Text = " Registrar ENTRADA";
+            }
+        }
+
+        private void rbSalida_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbSalida.Checked)
+            {
+                btnRegistrar.BackColor = System.Drawing.Color.LightCoral;
+                btnRegistrar.Text = " Registrar SALIDA";
+            }
+        }
+
+        // ========== EVENTOS VACÍOS (no necesitan código) ==========
+        private void dgvMovimientos_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+        private void gbDatosMovimiento_Enter(object sender, EventArgs e) { }
+        private void lblStockActual_Click(object sender, EventArgs e) { }
+        private void nudCantidad_ValueChanged(object sender, EventArgs e) { }
+        private void gbTipoMovimiento_Enter(object sender, EventArgs e) { }
+        private void gbHistorial_Enter(object sender, EventArgs e) { }
     }
 }
