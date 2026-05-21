771246dc410e57fd5620e7dec21aa87222a716c6
Endso <eramirezt18@miumg.edu.gt>
2026-05-21 01:48:35 -0600

    form de categoria añadido

--- Almacen_El_Dorado/FrmCategorias.Designer.cs
+++ Almacen_El_Dorado/FrmCategorias.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lstCategorias = new System.Windows.Forms.ListBox();
+            this.lblIntruccion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(42, 56);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(109, 16);
+            this.lblCategoria.TabIndex = 0;
+            this.lblCategoria.Text = "Nueva Categoria";
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Location = new System.Drawing.Point(187, 56);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(261, 22);
+            this.txtCategoria.TabIndex = 1;
+            this.txtCategoria.TextChanged += new System.EventHandler(this.txtCategoria_TextChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(254, 93);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(135, 29);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(80, 330);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(260, 28);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // lstCategorias
+            // 
+            this.lstCategorias.FormattingEnabled = true;
+            this.lstCategorias.ItemHeight = 16;
+            this.lstCategorias.Location = new System.Drawing.Point(45, 128);
+            this.lstCategorias.Name = "lstCategorias";
+            this.lstCategorias.Size = new System.Drawing.Size(577, 132);
+            this.lstCategorias.TabIndex = 4;
+            this.lstCategorias.SelectedIndexChanged += new System.EventHandler(this.lstCategorias_SelectedIndexChanged);
+            // 
+            // lblIntruccion
+            // 
+            this.lblIntruccion.AutoSize = true;
+            this.lblIntruccion.Location = new System.Drawing.Point(94, 298);
+            this.lblIntruccion.Name = "lblIntruccion";
+            this.lblIntruccion.Size = new System.Drawing.Size(222, 16);
+            this.lblIntruccion.TabIndex = 5;
+            this.lblIntruccion.Text = "Seleccione una categoria a Eliminar";
+            // 
+            // FrmCategorias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblIntruccion);
+            this.Controls.Add(this.lstCategorias);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtCategoria);
+            this.Controls.Add(this.lblCategoria);
+            this.Name = "FrmCategorias";
             this.Text = "FrmCategorias";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.ListBox lstCategorias;
+        private System.Windows.Forms.Label lblIntruccion;
     }
 }
--- Almacen_El_Dorado/FrmCategorias.cs
+++ Almacen_El_Dorado/FrmCategorias.cs
@@ -1,20 +1,146 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Almacen_El_Dorado
 {
     public partial class FrmCategorias : Form
     {
+        // Lista de categorias
+        List<string> listaCategorias = new List<string>();
+
+        // ========== CONSTRUCTOR ==========
         public FrmCategorias()
         {
             InitializeComponent();
+
+            // Cargar categorias de ejemplo
+            CargarCategoriasEjemplo();
+
+            // Mostrar categorias en el ListBox
+            MostrarCategorias();
+        }
+
+        // ========== CARGAR CATEGORIAS DE EJEMPLO ==========
+        private void CargarCategoriasEjemplo()
+        {
+            listaCategorias.Clear();
+            listaCategorias.Add("Electronica");
+            listaCategorias.Add("Ropa");
+            listaCategorias.Add("Alimentos");
+            listaCategorias.Add("Hogar");
+            listaCategorias.Add("Papeleria");
+            listaCategorias.Add("Herramientas");
+        }
+
+        // ========== MOSTRAR CATEGORIAS EN EL LISTBOX ==========
+        private void MostrarCategorias()
+        {
+            lstCategorias.Items.Clear();
+
+            foreach (string categoria in listaCategorias)
+            {
+                lstCategorias.Items.Add(categoria);
+            }
+        }
+
+        // ========== AGREGAR CATEGORIA ==========
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            // Validar que no este vacio
+            if (txtCategoria.Text == "")
+            {
+                MessageBox.Show("Ingrese el nombre de la categoria",
+                    "Validacion",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                txtCategoria.Focus();
+                return;
+            }
+
+            // Validar que no exista ya
+            foreach (string cat in listaCategorias)
+            {
+                if (cat.ToLower() == txtCategoria.Text.ToLower())
+                {
+                    MessageBox.Show("La categoria '" + txtCategoria.Text + "' ya existe",
+                        "Categoria duplicada",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning);
+                    txtCategoria.Text = "";
+                    txtCategoria.Focus();
+                    return;
+                }
+            }
+
+            // Guardar nombre para el mensaje
+            string nombreCategoria = txtCategoria.Text;
+
+            // Agregar nueva categoria
+            listaCategorias.Add(txtCategoria.Text);
+
+            // Actualizar lista
+            MostrarCategorias();
+
+            // Limpiar campo
+            txtCategoria.Text = "";
+            txtCategoria.Focus();
+
+            MessageBox.Show("Categoria '" + nombreCategoria + "' agregada correctamente",
+                "Exito",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        // ========== ELIMINAR CATEGORIA ==========
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            // Verificar que haya una categoria seleccionada
+            if (lstCategorias.SelectedIndex == -1)
+            {
+                MessageBox.Show("Seleccione una categoria para eliminar",
+                    "Aviso",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Obtener la categoria seleccionada
+            string categoriaSeleccionada = lstCategorias.SelectedItem.ToString();
+
+            // Confirmar eliminacion
+            DialogResult respuesta = MessageBox.Show("Esta seguro de eliminar la categoria:\n\n" +
+                categoriaSeleccionada + "\n\nEsta accion no se puede deshacer.",
+                "Confirmar eliminacion",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (respuesta == DialogResult.Yes)
+            {
+                // Eliminar de la lista
+                listaCategorias.RemoveAt(lstCategorias.SelectedIndex);
+
+                // Actualizar lista
+                MostrarCategorias();
+
+                MessageBox.Show("Categoria eliminada correctamente",
+                    "Exito",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        // ========== EVENTO CUANDO SE SELECCIONA UNA CATEGORIA ==========
+        private void lstCategorias_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Este evento se ejecuta cuando se selecciona un item
+            // No necesita codigo adicional, pero lo dejamos para referencia
+        }
+
+        // ========== EVENTO CUANDO CAMBIA EL TEXTO (no necesita codigo) ==========
+        private void txtCategoria_TextChanged(object sender, EventArgs e)
+        {
+            // No necesita codigo
         }
     }
 }
