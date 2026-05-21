a06dbc4959b552a29fe0aa3843145a9b19d7a73c
Endso <eramirezt18@miumg.edu.gt>
2026-05-21 02:14:05 -0600

    form proveedores añadidos

--- Almacen_El_Dorado/FrmProveedores.Designer.cs
+++ Almacen_El_Dorado/FrmProveedores.Designer.cs
@@ -28,12 +28,270 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbDatosProveedor = new System.Windows.Forms.GroupBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.lblContacto = new System.Windows.Forms.Label();
+            this.txtContacto = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lblBuscar = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.dgvProveedores = new System.Windows.Forms.DataGridView();
+            this.gbDatosProveedor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProveedores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbDatosProveedor
+            // 
+            this.gbDatosProveedor.Controls.Add(this.txtEmail);
+            this.gbDatosProveedor.Controls.Add(this.lblEmail);
+            this.gbDatosProveedor.Controls.Add(this.txtContacto);
+            this.gbDatosProveedor.Controls.Add(this.lblContacto);
+            this.gbDatosProveedor.Controls.Add(this.txtDireccion);
+            this.gbDatosProveedor.Controls.Add(this.lblDireccion);
+            this.gbDatosProveedor.Controls.Add(this.txtTelefono);
+            this.gbDatosProveedor.Controls.Add(this.lblTelefono);
+            this.gbDatosProveedor.Controls.Add(this.txtNombre);
+            this.gbDatosProveedor.Controls.Add(this.lblNombre);
+            this.gbDatosProveedor.Location = new System.Drawing.Point(37, 12);
+            this.gbDatosProveedor.Name = "gbDatosProveedor";
+            this.gbDatosProveedor.Size = new System.Drawing.Size(471, 212);
+            this.gbDatosProveedor.TabIndex = 0;
+            this.gbDatosProveedor.TabStop = false;
+            this.gbDatosProveedor.Text = "Datos del Proveedor";
+            this.gbDatosProveedor.Enter += new System.EventHandler(this.gbDatosProveedor_Enter);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(6, 31);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(59, 16);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(133, 28);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(311, 22);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(6, 65);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(64, 16);
+            this.lblTelefono.TabIndex = 2;
+            this.lblTelefono.Text = "Telefono:";
+            this.lblTelefono.Click += new System.EventHandler(this.lblTelefono_Click);
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(133, 62);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(311, 22);
+            this.txtTelefono.TabIndex = 3;
+            this.txtTelefono.TextChanged += new System.EventHandler(this.txtTelefono_TextChanged);
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(6, 91);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(67, 16);
+            this.lblDireccion.TabIndex = 4;
+            this.lblDireccion.Text = "Direccion:";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(133, 85);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(311, 22);
+            this.txtDireccion.TabIndex = 5;
+            this.txtDireccion.TextChanged += new System.EventHandler(this.txtDireccion_TextChanged);
+            // 
+            // lblContacto
+            // 
+            this.lblContacto.AutoSize = true;
+            this.lblContacto.Location = new System.Drawing.Point(6, 116);
+            this.lblContacto.Name = "lblContacto";
+            this.lblContacto.Size = new System.Drawing.Size(114, 16);
+            this.lblContacto.TabIndex = 6;
+            this.lblContacto.Text = "Persona Contacto";
+            // 
+            // txtContacto
+            // 
+            this.txtContacto.Location = new System.Drawing.Point(135, 113);
+            this.txtContacto.Name = "txtContacto";
+            this.txtContacto.Size = new System.Drawing.Size(309, 22);
+            this.txtContacto.TabIndex = 7;
+            this.txtContacto.TextChanged += new System.EventHandler(this.txtContacto_TextChanged);
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(12, 146);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(44, 16);
+            this.lblEmail.TabIndex = 8;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(133, 143);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(311, 22);
+            this.txtEmail.TabIndex = 9;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(527, 12);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 1;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(527, 43);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(527, 77);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(66, 26);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(527, 109);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(527, 152);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(85, 22);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lblBuscar
+            // 
+            this.lblBuscar.AutoSize = true;
+            this.lblBuscar.Location = new System.Drawing.Point(55, 264);
+            this.lblBuscar.Name = "lblBuscar";
+            this.lblBuscar.Size = new System.Drawing.Size(52, 16);
+            this.lblBuscar.TabIndex = 6;
+            this.lblBuscar.Text = "Buscar:";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(131, 261);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(263, 22);
+            this.txtBuscar.TabIndex = 7;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(400, 260);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 8;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // dgvProveedores
+            // 
+            this.dgvProveedores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProveedores.Location = new System.Drawing.Point(12, 301);
+            this.dgvProveedores.Name = "dgvProveedores";
+            this.dgvProveedores.RowHeadersWidth = 51;
+            this.dgvProveedores.RowTemplate.Height = 24;
+            this.dgvProveedores.Size = new System.Drawing.Size(907, 321);
+            this.dgvProveedores.TabIndex = 9;
+            this.dgvProveedores.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProveedores_CellContentClick);
+            // 
+            // FrmProveedores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(931, 634);
+            this.Controls.Add(this.dgvProveedores);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.lblBuscar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.gbDatosProveedor);
+            this.Name = "FrmProveedores";
             this.Text = "FrmProveedores";
+            this.gbDatosProveedor.ResumeLayout(false);
+            this.gbDatosProveedor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProveedores)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbDatosProveedor;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblContacto;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtContacto;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lblBuscar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.DataGridView dgvProveedores;
     }
 }
--- Almacen_El_Dorado/FrmProveedores.cs
+++ Almacen_El_Dorado/FrmProveedores.cs
@@ -1,20 +1,351 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Almacen_El_Dorado
 {
     public partial class FrmProveedores : Form
     {
+        // Lista de proveedores
+        List<Proveedor> listaProveedores = new List<Proveedor>();
+
+        // Variables para controlar edicion
+        bool editando = false;
+        int idActual = 0;
+
+        // ========== CONSTRUCTOR ==========
         public FrmProveedores()
         {
             InitializeComponent();
+
+            // Configurar DataGridView
+            ConfigurarDataGridView();
+
+            // Cargar proveedores de ejemplo
+            CargarProveedoresEjemplo();
+
+            // Mostrar proveedores
+            MostrarProveedores();
         }
+
+        // ========== CLASE PROVEEDOR ==========
+        class Proveedor
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public string Telefono { get; set; }
+            public string Direccion { get; set; }
+            public string Contacto { get; set; }
+            public string Email { get; set; }
+        }
+
+        // ========== CONFIGURAR DATAGRIDVIEW ==========
+        private void ConfigurarDataGridView()
+        {
+            dgvProveedores.Columns.Clear();
+
+            dgvProveedores.Columns.Add("Id", "ID");
+            dgvProveedores.Columns.Add("Nombre", "Nombre");
+            dgvProveedores.Columns.Add("Telefono", "Telefono");
+            dgvProveedores.Columns.Add("Direccion", "Direccion");
+            dgvProveedores.Columns.Add("Contacto", "Contacto");
+            dgvProveedores.Columns.Add("Email", "Email");
+
+            dgvProveedores.Columns["Id"].Width = 50;
+            dgvProveedores.Columns["Nombre"].Width = 150;
+            dgvProveedores.Columns["Telefono"].Width = 100;
+            dgvProveedores.Columns["Direccion"].Width = 150;
+            dgvProveedores.Columns["Contacto"].Width = 120;
+            dgvProveedores.Columns["Email"].Width = 150;
+
+            dgvProveedores.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvProveedores.MultiSelect = false;
+            dgvProveedores.AllowUserToAddRows = false;
+        }
+
+        // ========== CARGAR PROVEEDORES DE EJEMPLO ==========
+        private void CargarProveedoresEjemplo()
+        {
+            listaProveedores.Clear();
+
+            listaProveedores.Add(new Proveedor
+            {
+                Id = 1,
+                Nombre = "ElectroTech",
+                Telefono = "12345678",
+                Direccion = "Zona 1, Ciudad",
+                Contacto = "Juan Perez",
+                Email = "ventas@electrotech.com"
+            });
+
+            listaProveedores.Add(new Proveedor
+            {
+                Id = 2,
+                Nombre = "Moda Express",
+                Telefono = "87654321",
+                Direccion = "Zona 4, Ciudad",
+                Contacto = "Maria Lopez",
+                Email = "info@modaexpress.com"
+            });
+
+            listaProveedores.Add(new Proveedor
+            {
+                Id = 3,
+                Nombre = "Distribuidora de Alimentos",
+                Telefono = "55555555",
+                Direccion = "Zona 7, Ciudad",
+                Contacto = "Carlos Ruiz",
+                Email = "carlos@distalimentos.com"
+            });
+        }
+
+        // ========== MOSTRAR PROVEEDORES ==========
+        private void MostrarProveedores(string filtro = "")
+        {
+            dgvProveedores.Rows.Clear();
+
+            int fila = 0;
+            foreach (Proveedor p in listaProveedores)
+            {
+                // Aplicar filtro si existe
+                if (filtro != "")
+                {
+                    if (!p.Nombre.ToLower().Contains(filtro.ToLower()) &&
+                        !p.Telefono.Contains(filtro) &&
+                        !p.Contacto.ToLower().Contains(filtro.ToLower()))
+                    {
+                        continue;
+                    }
+                }
+
+                dgvProveedores.Rows.Add();
+                dgvProveedores.Rows[fila].Cells[0].Value = p.Id;
+                dgvProveedores.Rows[fila].Cells[1].Value = p.Nombre;
+                dgvProveedores.Rows[fila].Cells[2].Value = p.Telefono;
+                dgvProveedores.Rows[fila].Cells[3].Value = p.Direccion;
+                dgvProveedores.Rows[fila].Cells[4].Value = p.Contacto;
+                dgvProveedores.Rows[fila].Cells[5].Value = p.Email;
+
+                fila++;
+            }
+        }
+
+        // ========== LIMPIAR CAMPOS ==========
+        private void LimpiarCampos()
+        {
+            txtNombre.Text = "";
+            txtTelefono.Text = "";
+            txtDireccion.Text = "";
+            txtContacto.Text = "";
+            txtEmail.Text = "";
+
+            editando = false;
+            idActual = 0;
+
+            txtNombre.Focus();
+        }
+
+        // ========== VALIDAR CAMPOS ==========
+        private bool ValidarCampos()
+        {
+            if (txtNombre.Text == "")
+            {
+                MessageBox.Show("Ingrese el nombre del proveedor", "Validacion",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNombre.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        // ========== BOTON NUEVO ==========
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+            dgvProveedores.ClearSelection();
+        }
+
+        // ========== BOTON GUARDAR ==========
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (!ValidarCampos())
+                return;
+
+            if (!editando)
+            {
+                // Agregar nuevo proveedor
+                int nuevoId = 1;
+                if (listaProveedores.Count > 0)
+                {
+                    nuevoId = listaProveedores[listaProveedores.Count - 1].Id + 1;
+                }
+
+                Proveedor nuevo = new Proveedor();
+                nuevo.Id = nuevoId;
+                nuevo.Nombre = txtNombre.Text;
+                nuevo.Telefono = txtTelefono.Text;
+                nuevo.Direccion = txtDireccion.Text;
+                nuevo.Contacto = txtContacto.Text;
+                nuevo.Email = txtEmail.Text;
+
+                listaProveedores.Add(nuevo);
+
+                MessageBox.Show("Proveedor guardado correctamente", "Exito",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                // Editar proveedor existente
+                for (int i = 0; i < listaProveedores.Count; i++)
+                {
+                    if (listaProveedores[i].Id == idActual)
+                    {
+                        listaProveedores[i].Nombre = txtNombre.Text;
+                        listaProveedores[i].Telefono = txtTelefono.Text;
+                        listaProveedores[i].Direccion = txtDireccion.Text;
+                        listaProveedores[i].Contacto = txtContacto.Text;
+                        listaProveedores[i].Email = txtEmail.Text;
+                        break;
+                    }
+                }
+
+                MessageBox.Show("Proveedor actualizado correctamente", "Exito",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                editando = false;
+                idActual = 0;
+            }
+
+            MostrarProveedores();
+            LimpiarCampos();
+        }
+
+        // ========== BOTON EDITAR ==========
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dgvProveedores.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Seleccione un proveedor para editar", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int id = Convert.ToInt32(dgvProveedores.SelectedRows[0].Cells[0].Value);
+
+            foreach (Proveedor p in listaProveedores)
+            {
+                if (p.Id == id)
+                {
+                    txtNombre.Text = p.Nombre;
+                    txtTelefono.Text = p.Telefono;
+                    txtDireccion.Text = p.Direccion;
+                    txtContacto.Text = p.Contacto;
+                    txtEmail.Text = p.Email;
+
+                    editando = true;
+                    idActual = p.Id;
+                    break;
+                }
+            }
+
+            txtNombre.Focus();
+        }
+
+        // ========== BOTON ELIMINAR ==========
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvProveedores.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Seleccione un proveedor para eliminar", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string nombreProveedor = dgvProveedores.SelectedRows[0].Cells[1].Value.ToString();
+
+            DialogResult respuesta = MessageBox.Show("Esta seguro de eliminar al proveedor:\n\n" +
+                nombreProveedor + "\n\nEsta accion no se puede deshacer.",
+                "Confirmar eliminacion",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (respuesta == DialogResult.Yes)
+            {
+                int id = Convert.ToInt32(dgvProveedores.SelectedRows[0].Cells[0].Value);
+
+                Proveedor eliminar = null;
+                foreach (Proveedor p in listaProveedores)
+                {
+                    if (p.Id == id)
+                    {
+                        eliminar = p;
+                        break;
+                    }
+                }
+
+                if (eliminar != null)
+                {
+                    listaProveedores.Remove(eliminar);
+                    MessageBox.Show("Proveedor eliminado correctamente", "Exito",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    MostrarProveedores();
+                    LimpiarCampos();
+                }
+            }
+        }
+
+        // ========== BOTON LIMPIAR ==========
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+            MostrarProveedores();
+            txtBuscar.Text = "";
+            dgvProveedores.ClearSelection();
+        }
+
+        // ========== BOTON BUSCAR ==========
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string textoBuscar = txtBuscar.Text.Trim();
+            if (textoBuscar == "")
+            {
+                MostrarProveedores();
+            }
+            else
+            {
+                MostrarProveedores(textoBuscar);
+            }
+        }
+
+        // ========== BUSCAR CON ENTER ==========
+        private void txtBuscar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btnBuscar_Click(sender, e);
+            }
+        }
+
+        // ========== CLICK EN DATAGRIDVIEW ==========
+        private void dgvProveedores_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                dgvProveedores.Rows[e.RowIndex].Selected = true;
+            }
+        }
+
+        // ========== EVENTOS VACIOS (no necesitan codigo) ==========
+        private void lblTelefono_Click(object sender, EventArgs e) { }
+        private void txtBuscar_TextChanged(object sender, EventArgs e) { }
+        private void dgvProveedores_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+        private void txtNombre_TextChanged(object sender, EventArgs e) { }
+        private void txtTelefono_TextChanged(object sender, EventArgs e) { }
+        private void txtDireccion_TextChanged(object sender, EventArgs e) { }
+        private void txtContacto_TextChanged(object sender, EventArgs e) { }
+        private void txtEmail_TextChanged(object sender, EventArgs e) { }
+        private void gbDatosProveedor_Enter(object sender, EventArgs e) { }
     }
 }
